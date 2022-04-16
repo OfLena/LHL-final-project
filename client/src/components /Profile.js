d32bb1b22cb1e3ff4ac2df97bee965c9c6cac17f
scee10 <schhor05@gmail.com>
2022-04-16 12:35:50 -0400

    Update profile: Allow different HTML to show depending on button clicked

--- client/src/components /Profile.js
+++ client/src/components /Profile.js
@@ -4,20 +4,21 @@ import axios from 'axios';
 export default function Profile (props) {
 
 const { users, recipes, favs } = props
-
-console.log(recipes)
-console.log(users)
-console.log(favs)
+const [showFavs, setShowFavs] = useState("")
+const [showUserRecipes, setShowUserRecipes] = useState("")
 
 function getFavRecipes() {
-// axios get 
+  setShowUserRecipes("")
   return(
-    <li>{favs.map(fav => fav.recipe_id)}</li>
+    setShowFavs(<li>{favs.map(fav => fav.recipe_id)}</li>) 
   )
 }
 
 function getUserRecipes() {
-  // recipes.map(recipe => console.log({recipe.title}))
+  setShowFavs("")
+  return(
+    setShowUserRecipes(<li>{recipes.map(recipe => recipe.title)}</li>) 
+  )
 }
   
   return (
@@ -26,7 +27,6 @@ function getUserRecipes() {
       {/* Need to add logic here to populate first name, last name and user name where it matches the id of the user */}
 
       <ul>
-      <li>{recipes.map(recipe => recipe.title)}</li>
       <li>{users.map(user => user.first_name)}</li>
       <li>{users.map(user => user.last_name)}</li>
       <li>{users.map(user => user.user_name)}</li>
@@ -35,9 +35,10 @@ function getUserRecipes() {
       <input type= "button" value="Fav Recipes" onClick={getFavRecipes}/>
       <input type= "button" value="Your Recipes" onClick={getUserRecipes}/>
       <li><input type="text" placeholder="Search Recipes..."></input></li>
+      <h1>{showFavs}</h1>
+      <h1>{showUserRecipes}</h1>
 
       {/* // area to populate either saved recipes or your recipes */}
-
     </div>
   )
 }
