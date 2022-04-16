925ac39de85d5087712c69997dfb5c5f35192377
Guy Dilena <guydilena@gmail.com>
2022-04-16 09:32:30 -0300

    Add: Recipe Seeds

--- server/db/seeds/04_recipes.sql
+++ server/db/seeds/04_recipes.sql
@@ -9,6 +9,7 @@
  'Potatoes', 'Onions', 'Olive oil', 'Chicken stock', 'Creme Fraiche', 'Topping Parmesan', 'Chicken Breasts', 'Bacon', 'Topping Spinach', null, null, null, null, null, null, null, null, null, null, null,
   '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', '1 cup', null, null, null, null, null, null, null, null, null, null),
  
+
  (2, 3, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/rvxxuy1468312893.jpg', 
  'Vegan Lasagna',
  'Preheat oven to 180 degrees celcius.',
@@ -19,15 +20,98 @@
   When cooked, drain and set aside.',
   'To make the sauce, melt the butter and add the flour, then gradually add the soya milk along with the mustard and the vinegar.
   Cook and stir until smooth and then assemble the lasagne as desired in a baking dish.', 
- 'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null, '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null);
+ 'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null,
+  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null),
  
---  (3, 4, 'youtube.com', '45 minutes', 2, 'https://www.themealdb.com/images/media/meals/qwrtut1468418027.jpg', 
---  'Burritos',
---  'Finely slice 3 onions and place in an oven dish with 2 tblsp of olive oil and 100ml of chicken stock.',
---  'Cook till the onions are soft then drain the potatoes and pour onto the onions.',
---  'Season and spoon over cream or creme fraiche till all is covered but not swimming.',
---  'Grate Parmesan over the top then finish under the grill till nicely golden.',
---  'serve with chicken and bacon, peas and spinach.',
---  'Potatoes', 'Onions', 'Olive oil', 'Chicken stock', 'Creme Fraiche', 'Topping Parmesan', 'Chicken Breasts', 'Bacon', 'Topping Spinach', '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', '1 cup');
+
+ (3, 4, 'youtube.com', '45 minutes', 2, 'https://i0.wp.com/www.wearesovegan.com/wp-content/uploads/2019/10/veganindianchickpeatikkaburrito-h1.jpg?w=1280&ssl=1', 
+ 'Burritos',
+ 'Cube a sweet potato and place on baking sheet with cinnamom and olive oil at 350F for 40mins. Flip once at 20mins.',
+ 'Chop a medium yellow onion and 3 gloves of garlic and place in hot brazier at medium heat. Cook until onions are translucent. Start your desired amount of rice.',
+ 'Add salt, pepper, coriander, cumin, cayenne, chili powder, and tumeric and cook for another min. Add your choice of greens and pinto beans',
+ 'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
+ 'Serve.',
+ 'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
+  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null),
 
 
+(1, 2, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/qwrtut1468418027.jpg', 
+ 'Potato Gratin with Chicken',
+ 'Finely slice 3 onions and place in an oven dish with 2 tblsp of olive oil and 100ml of chicken stock.',
+ 'Cook till the onions are soft then drain the potatoes and pour onto the onions.',
+ 'Season and spoon over cream or creme fraiche till all is covered but not swimming.',
+ 'Grate Parmesan over the top then finish under the grill till nicely golden.',
+ 'serve with chicken and bacon, peas and spinach.',
+ 'Potatoes', 'Onions', 'Olive oil', 'Chicken stock', 'Creme Fraiche', 'Topping Parmesan', 'Chicken Breasts', 'Bacon', 'Topping Spinach', null, null, null, null, null, null, null, null, null, null, null,
+  '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', '1 cup', null, null, null, null, null, null, null, null, null, null),
+ 
+
+ (2, 3, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/rvxxuy1468312893.jpg', 
+ 'Vegan Lasagna',
+ 'Preheat oven to 180 degrees celcius.',
+ 'Boil vegetables for 5-7 minutes, until soft. Add lentils and bring to a gentle simmer, adding a stock cube if desired.
+  Continue cooking and stirring until the lentils are soft, which should take about 20 minutes.',
+ 'Blanch spinach leaves for a few minutes in a pan, before removing and setting aside.',
+ 'Top up the pan with water and cook the lasagne sheets.
+  When cooked, drain and set aside.',
+  'To make the sauce, melt the butter and add the flour, then gradually add the soya milk along with the mustard and the vinegar.
+  Cook and stir until smooth and then assemble the lasagne as desired in a baking dish.', 
+ 'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null,
+  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null),
+ 
+
+ (3, 4, 'youtube.com', '45 minutes', 2, 'https://i0.wp.com/www.wearesovegan.com/wp-content/uploads/2019/10/veganindianchickpeatikkaburrito-h1.jpg?w=1280&ssl=1', 
+ 'Burritos',
+ 'Cube a sweet potato and place on baking sheet with cinnamom and olive oil at 350F for 40mins. Flip once at 20mins.',
+ 'Chop a medium yellow onion and 3 gloves of garlic and place in hot brazier at medium heat. Cook until onions are translucent. Start your desired amount of rice.',
+ 'Add salt, pepper, coriander, cumin, cayenne, chili powder, and tumeric and cook for another min. Add your choice of greens and pinto beans',
+ 'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
+ 'Serve.',
+ 'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
+  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null),
+
+(1, 2, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/qwrtut1468418027.jpg', 
+ 'Potato Gratin with Chicken',
+ 'Finely slice 3 onions and place in an oven dish with 2 tblsp of olive oil and 100ml of chicken stock.',
+ 'Cook till the onions are soft then drain the potatoes and pour onto the onions.',
+ 'Season and spoon over cream or creme fraiche till all is covered but not swimming.',
+ 'Grate Parmesan over the top then finish under the grill till nicely golden.',
+ 'serve with chicken and bacon, peas and spinach.',
+ 'Potatoes', 'Onions', 'Olive oil', 'Chicken stock', 'Creme Fraiche', 'Topping Parmesan', 'Chicken Breasts', 'Bacon', 'Topping Spinach', null, null, null, null, null, null, null, null, null, null, null,
+  '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', '1 cup', null, null, null, null, null, null, null, null, null, null),
+ 
+
+ (2, 3, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/rvxxuy1468312893.jpg', 
+ 'Vegan Lasagna',
+ 'Preheat oven to 180 degrees celcius.',
+ 'Boil vegetables for 5-7 minutes, until soft. Add lentils and bring to a gentle simmer, adding a stock cube if desired.
+  Continue cooking and stirring until the lentils are soft, which should take about 20 minutes.',
+ 'Blanch spinach leaves for a few minutes in a pan, before removing and setting aside.',
+ 'Top up the pan with water and cook the lasagne sheets.
+  When cooked, drain and set aside.',
+  'To make the sauce, melt the butter and add the flour, then gradually add the soya milk along with the mustard and the vinegar.
+  Cook and stir until smooth and then assemble the lasagne as desired in a baking dish.', 
+ 'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null,
+  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null),
+ 
+
+ (3, 4, 'youtube.com', '45 minutes', 2, 'https://i0.wp.com/www.wearesovegan.com/wp-content/uploads/2019/10/veganindianchickpeatikkaburrito-h1.jpg?w=1280&ssl=1', 
+ 'Burritos',
+ 'Cube a sweet potato and place on baking sheet with cinnamom and olive oil at 350F for 40mins. Flip once at 20mins.',
+ 'Chop a medium yellow onion and 3 gloves of garlic and place in hot brazier at medium heat. Cook until onions are translucent. Start your desired amount of rice.',
+ 'Add salt, pepper, coriander, cumin, cayenne, chili powder, and tumeric and cook for another min. Add your choice of greens and pinto beans',
+ 'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
+ 'Serve.',
+ 'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
+  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null);
+
+
+-- (1, 2, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/qwrtut1468418027.jpg', 
+--  'title',
+--  'Step1',
+--  null,
+--  null,
+--  null,
+--  null,
+--  'Potatoes', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+--   '1 TBSP', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null),
