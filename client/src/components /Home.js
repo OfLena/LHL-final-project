44a39b8af2a33e5b87dcfb18c215d5cfcda61e6f
Guy Dilena <guydilena@gmail.com>
2022-04-15 21:35:40 -0300

    Style Home Page/Recipe-Card

--- client/src/components /Home.js
+++ client/src/components /Home.js
@@ -1,15 +1,31 @@
 import "./styles/recipe.scss";
 
+import RecipeListItem from "./RecipeListItem";
+
 export default function Home(props) {
   
-  const { recipes } = props;
-  
+console.log('PROPS -->', props.recipes)
+
+  const recipes = props.recipes.map(recipe => {
+    return (
+      <RecipeListItem
+      key={recipe.id}
+      title={recipe.title}
+      image_url={recipe.image_url}
+      prep_time={recipe.prep_time}
+      />
+    );
+  });
+
   return (
     
     <div class="recipe-container">
-      <ul class="recipe-card-list">
+      <img class="cover-img" src="https://cdn.stocksnap.io/img-thumbs/960w/pizza-wine_IJESKJTYB6.jpg"></img>
+      <div class="search-bar"><input type="text" placeholder="Search.."></input></div>
+      <div class="centered">Bring Out Your Inner Chef</div>
+      <div class="recipe-card-list-item">
         {recipes}
-      </ul>
+      </div>
     </div>
     
   );
