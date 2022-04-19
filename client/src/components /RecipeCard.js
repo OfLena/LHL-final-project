3fb0020db07dc05c0669cc6ef2af95bb9f7c8d42
Guy Dilena <guydilena@gmail.com>
2022-04-19 20:34:57 -0300

    Style Recipe Card

--- client/src/components /RecipeCard.js
+++ client/src/components /RecipeCard.js
@@ -17,12 +17,10 @@ import {
   Card,
 } from "@mui/material";
 
-import KitchenIcon from '@mui/icons-material/Kitchen';
-import ScaleIcon from '@mui/icons-material/Scale';
+import KitchenIcon from "@mui/icons-material/Kitchen";
+import ScaleIcon from "@mui/icons-material/Scale";
 
-
-
-
+import Footer from "./Footer";
 
 export default function RecipeCard(props) {
   const { recipes, currentPage, setCurrentPage } = props;
@@ -34,25 +32,27 @@ export default function RecipeCard(props) {
 
     const recipePairs = Object.entries(filteredRecipes);
 
-      
-
     if (filteredRecipes.id === currentPage) {
       return (
-        <Box key={index}
+        <Box
+          key={index}
           marginTop={"8rem"}
-          marginBottom={"8rem"}
+          marginBottom={"2rem"}
           sx={{ p: 2, border: "1px dashed grey", backgroundColor: "#CCA01D" }}
         >
           <Card sx={{ paddingBottom: "2rem", bgcolor: "beige" }}>
             <CardHeader
               title={filteredRecipes.title}
               avatar={
-                <Avatar sx={{ bgcolor: 'black' }} aria-label="recipe">
+                <Avatar sx={{ bgcolor: "black" }} aria-label="recipe">
                   R
                 </Avatar>
               }
             />
-            <div className="top-card"style={{ display: "flex", justifyContent: "center", }}>
+            <div
+              className="top-card"
+              style={{ display: "flex", justifyContent: "center" }}
+            >
               <CardMedia
                 component="img"
                 sx={{ maxWidth: 600 }}
@@ -63,51 +63,72 @@ export default function RecipeCard(props) {
             </div>
           </Card>
 
-          <Typography variant='h5'>Prep Time {filteredRecipes.prep_time}</Typography>
-          <Typography variant='h6' >Serves {filteredRecipes.serving_size}</Typography>
-          
-          <div className="map-container">
-            
-          <div>
-             
+          <Typography variant="h5">
+            Prep Time {filteredRecipes.prep_time}
+          </Typography>
+          <Typography variant="h6">
+            Serves {filteredRecipes.serving_size}
+          </Typography>
+
+          <Grid container className="map-container">
+            <Grid>
+              <Typography>Ingredients</Typography>
               {recipePairs.map((value, index) => {
                 let ingredient;
                 if (value[0].includes("ingredient")) {
                   ingredient = value[1];
-                  return <div key={index} className='value-container'><KitchenIcon/>{ingredient}</div>
+                  return (
+                    <Grid item key={index}>
+                      <KitchenIcon /> {ingredient}
+                    </Grid>
+                  );
                 }
               })}
-
-              
-
-            </div>
-              <div>
+            </Grid>
+            <Grid>
+              <Typography>Measurements</Typography>
               {recipePairs.map((value, index) => {
                 let measurement;
                 if (value[0].includes("measurement")) {
                   measurement = value[1];
-                  return <div key={index} alignSelf='left' className="value-container"><ScaleIcon/>{measurement}</div>
+                  return (
+                    <Grid
+                      item
+                      key={index}
+                      alignSelf="left"
+                      
+                    >
+                      <ScaleIcon /> {measurement}
+                    </Grid>
+                  );
                 }
-                
               })}
-            </div>
-            </div>
-     
-              <Typography>Instructions</Typography>
-              <div className="instruction-container">
+            </Grid>
+
+            
+
+            <Grid>
+            <Typography>Instructions</Typography>
               {recipePairs.map((value, index) => {
-              let instruction;
-              if (value[0].includes("instruction")) {
-                instruction = value[1];
-                return <div key={index}>{instruction}</div>;
-              }
-              
-            })}
-          </div>
+                let instruction;
+                if (value[0].includes("instruction")) {
+                  instruction = value[1];
+                  return (
+                    <Grid item key={index}>
+                      {instruction}
+                    </Grid>
+                  );
+                }
+              })}
+            </Grid>
+          </Grid>
         </Box>
       );
     }
   });
 
-  return <Container>{findRecipe}</Container>;
+  return <Container>
+    {findRecipe}
+    <Footer/>
+  </Container>;
 }
