6f1900356dc78b04d20b9cc09da625b74e1337de
Guy Dilena <guydilena@gmail.com>
2022-04-23 09:56:25 -0300

    Add: Recipe seeds

--- server/db/seeds/04_recipes.sql
+++ server/db/seeds/04_recipes.sql
@@ -1,4 +1,4 @@
- INSERT INTO recipes (id, user_id, prep_time, serving_size, image_url, title, instruction_1, instruction_2, instruction_3, instruction_4, instruction_5, ingredient_1, ingredient_2, ingredient_3, ingredient_4, ingredient_5, ingredient_6, ingredient_7, ingredient_8, ingredient_9, ingredient_10, ingredient_11, ingredient_12, ingredient_13, ingredient_14, ingredient_15, ingredient_16, ingredient_17, ingredient_18, ingredient_19, ingredient_20, measurement_1, measurement_2, measurement_3, measurement_4, measurement_5, measurement_6, measurement_7, measurement_8, measurement_9, measurement_10, measurement_11, measurement_12, measurement_13, measurement_14, measurement_15, measurement_16, measurement_17, measurement_18, measurement_19, measurement_20, vegan)
+ INSERT INTO recipes (id, user_id, prep_time, serving_size, image_url, title, instruction_1, instruction_2, instruction_3, instruction_4, instruction_5, ingredient_1, ingredient_2, ingredient_3, ingredient_4, ingredient_5, ingredient_6, ingredient_7, ingredient_8, ingredient_9, ingredient_10, ingredient_11, ingredient_12, ingredient_13, ingredient_14, ingredient_15, ingredient_16, ingredient_17, ingredient_18, ingredient_19, ingredient_20, measurement_1, measurement_2, measurement_3, measurement_4, measurement_5, measurement_6, measurement_7, measurement_8, measurement_9, measurement_10, measurement_11, measurement_12, measurement_13, measurement_14, measurement_15, measurement_16, measurement_17, measurement_18, measurement_19, measurement_20, vegan, vegetarian, keto, breakfast, lunch, dinner, gluten_free, dairy_free)
  VALUES (1, 4, '30 minutes', 4, 'potato-gratin.jpg', 
  'Potato Gratin with Chicken',
  'Finely slice 3 onions and place in an oven dish with 2 tblsp of olive oil and 100ml of chicken stock.',
@@ -7,7 +7,8 @@
  'Grate Parmesan over the top then finish under the grill till nicely golden.',
  'serve with chicken and bacon, peas and spinach.',
  'Potatoes', 'Onions', 'Olive oil', 'Chicken stock', 'Creme Fraiche', 'Topping Parmesan', 'Chicken Breasts', 'Bacon', 'Topping Spinach', null, null, null, null, null, null, null, null, null, null, null,
-  '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', null, null, null, null, null, null, null, null, null, null, null, TRUE),
+  '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', null, null, null, null, null, null, null, null, null, null, null,
+   FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE),
  
 
  (2, 2, '30 minutes', 4, 'vegan-lasagna.jpg', 
@@ -21,18 +22,20 @@
   'To make the sauce, melt the butter and add the flour, then gradually add the soya milk along with the mustard and the vinegar.
   Cook and stir until smooth and then assemble the lasagne as desired in a baking dish.', 
  'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null,
-  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null, TRUE),
+  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null,
+   TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE),
  
 
  (3, 3,'45 minutes', 2, 'burrito.jpg', 
- 'Burritos',
+ 'Vegan Burritos',
  'Cube a sweet potato and place on baking sheet with cinnamom and olive oil at 350F for 40mins. Flip once at 20mins.',
  'Chop a medium yellow onion and 3 gloves of garlic and place in hot brazier at medium heat. Cook until onions are translucent. Start your desired amount of rice.',
  'Add salt, pepper, coriander, cumin, cayenne, chili powder, and tumeric and cook for another min. Add your choice of greens and pinto beans',
  'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
  'Serve.',
  'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
-  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null, TRUE),
+  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null,
+   TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, TRUE),
 
 
 (4, 1, '30 minutes', 3, 'fish-taco.jpg', 
@@ -43,7 +46,8 @@
  'Cook in batches if you don’t have enough room in the pan.',
  'Meanwhile, prepare the dressing by combining all the ingredients with a little seasoning.',
  'Cajun', 'Cayenne Pepper', 'White fish', 'Vegetable Oil', 'Flour tortilla', 'Avocado', 'Shredded Lettuce', 'Spring Onion', 'Salsa', 'Sour cream', 'Lemon', 'Chopped garlic', null, null, null, null, null, null, null, null,
-  '2 tbsp', '1 tsp', '2 fillets', '1 tsp', '8', '1 sliced', '2', '4', '1 x 300ml', '1 pot', '1 lemon', '1 clove', null, null, null, null, null, null, null, null, TRUE),
+  '2 tbsp', '1 tsp', '2 fillets', '1 tsp', '8', '1 sliced', '2', '4', '1 x 300ml', '1 pot', '1 lemon', '1 clove', null, null, null, null, null, null, null, null,
+   FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE),
  
 
  (5, 2, '1 hour', 4, 'kfc.jpeg', 
@@ -57,7 +61,8 @@
   'Repeat with all the chicken pieces.
   Let chicken pieces rest for 5 minutes so crust has a chance to dry a bit. Fry chicken in batches.', 
  'Whole Chicken', 'Frying Oil', 'Egg white', 'Flour', 'Brown Sugar', 'Salt', 'Paprika', 'Onion salt', 'Chili powder', 'Black pepper', 'Celery salt', 'Sage', 'Garlic powder', 'Allspice', 'Oregano', null, null, null, null, null,
-  '1', '2 quarts', '1', '1 1/2 cups', '1 tablespoon', '1 tbsp', '1 tbsp', '2 tsp', '1 tsp', '1 tsp', '1/2 tsp', '1/2 tsp', '1/2 tsp', '1/2 tsp', '1/2 tsp', null, null, null, null, null, FALSE),
+  '1', '2 quarts', '1', '1 1/2 cups', '1 tablespoon', '1 tbsp', '1 tbsp', '2 tsp', '1 tsp', '1 tsp', '1/2 tsp', '1/2 tsp', '1/2 tsp', '1/2 tsp', '1/2 tsp', null, null, null, null, null,
+   FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE),
  
 
  (6, 3, '45 minutes', 2, 'burrito.jpg', 
@@ -68,7 +73,8 @@
  'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
  'Serve.',
  'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
-  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null, TRUE),
+  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null,
+   TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE),
 
 (7, 1, '30 minutes', 4, 'duck.png', 
  'Potato Gratin with Duck',
@@ -78,7 +84,8 @@
  'Grate Parmesan over the top then finish under the grill till nicely golden.',
  'serve with chicken and bacon, peas and spinach.',
  'Potatoes', 'Onions', 'Olive oil', 'Chicken stock', 'Creme Fraiche', 'Topping Parmesan', 'Chicken Breasts', 'Bacon', 'Topping Spinach', null, null, null, null, null, null, null, null, null, null, null,
-  '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', '1 cup', null, null, null, null, null, null, null, null, null, null, TRUE),
+  '800g', '3', '2 tbsp', '100ml', '1 pot', '2 tbsp', '4', '4', '1/4 cup', '1 cup', null, null, null, null, null, null, null, null, null, null,
+   FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE),
  
 
  (8, 2, '30 minutes', 4, 'garfield-lasagna.jpg', 
@@ -92,10 +99,22 @@
   'To make the sauce, melt the butter and add the flour, then gradually add the soya milk along with the mustard and the vinegar.
   Cook and stir until smooth and then assemble the lasagne as desired in a baking dish.', 
  'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null,
-  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null, TRUE),
+  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null,
+   FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE),
+ 
+ (9, 3, '5 minutes', 1, 'overnightOats.jpg', 
+ 'Vegan Overnight Oats',
+ 'In a small bowl, whish together the oats, almond milk, chia seeds, banana and cinnamon. Cover and refrigerate overnight to thicken.',
+ 'In the morning, stir the oat mixture to combine. Serve the oats in a jar, alternating layers of fresh fruit, granola, hemp seeds, and a drizzle of sweetener, if desired.',
+ null,
+ null,
+ null,
+ 'Rolled Oats', 'Almond Milk', 'Chia Seeds', 'Banana', 'Cinnamon', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+  '1 Cup', '1.5 Cups', '1/4 Cup', '1 Large', '1/2 TSP', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+  TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE),
  
 
- (9, 3,'45 minutes', 2, 'beans.jpg', 
+ (10, 3,'45 minutes', 2, 'beans.jpg', 
  'BEANS',
  'Cube a sweet potato and place on baking sheet with cinnamom and olive oil at 350F for 40mins. Flip once at 20mins.',
  'Chop a medium yellow onion and 3 gloves of garlic and place in hot brazier at medium heat. Cook until onions are translucent. Start your desired amount of rice.',
@@ -103,10 +122,11 @@
  'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
  'Serve.',
  'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
-  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null, TRUE);
+  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null,
+   TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE);
 
 
--- (1, 2, 'youtube.com', '30 minutes', 4, 'https://www.themealdb.com/images/media/meals/qwrtut1468418027.jpg', 
+-- (1, 2, '30 minutes', 4, '.jpg', 
 --  'title',
 --  'Step1',
 --  null,
@@ -114,4 +134,5 @@
 --  null,
 --  null,
 --  'Potatoes', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
---   '1 TBSP', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null),
+--   '1 TBSP', null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+-- FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE);
