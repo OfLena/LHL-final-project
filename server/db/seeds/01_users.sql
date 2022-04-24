83273fe6c3bae64dc2fb00e8ffb2ca50b6759d44
scee10 <schhor05@gmail.com>
2022-04-24 10:51:10 -0400

    reseeding database with new recipe

--- server/db/seeds/01_users.sql
+++ server/db/seeds/01_users.sql
@@ -1,5 +1,8 @@
 
 INSERT INTO users (user_name, first_name, last_name, email, password, avatar)
+
 VALUES ('LaufferN', 'Noah', 'Lauffer', 'lauffern@gmail.com', '1234', 'Avatar3.png'),
+
 ('DilenaG', 'Guy', 'Dilena', 'dilenaG@gmail.com', '1234', 'Avatar4.png'),
+
 ('ChhorS', 'Stef', 'Chhor', 'chhorS@gmail.com', '1234', 'Avatar5.png')
