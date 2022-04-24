e24cddbc5087558f9dc12ef88d8ab792a959e7f1
scee10 <schhor05@gmail.com>
2022-04-24 11:08:18 -0400

    add more recipes

--- server/db/seeds/04_recipes.sql
+++ server/db/seeds/04_recipes.sql
@@ -93,19 +93,20 @@ Drain and serve with sauce.',
    FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, FALSE, FALSE),
  
 
- (8, 2, '30 minutes', 4, 'garfield-lasagna.jpg', 
- 'Garfields Lasagna',
- 'Preheat oven to 180 degrees celcius.',
- 'Boil vegetables for 5-7 minutes, until soft. Add lentils and bring to a gentle simmer, adding a stock cube if desired.
-  Continue cooking and stirring until the lentils are soft, which should take about 20 minutes.',
- 'Blanch spinach leaves for a few minutes in a pan, before removing and setting aside.',
- 'Top up the pan with water and cook the lasagne sheets.
-  When cooked, drain and set aside.',
-  'To make the sauce, melt the butter and add the flour, then gradually add the soya milk along with the mustard and the vinegar.
-  Cook and stir until smooth and then assemble the lasagne as desired in a baking dish.', 
- 'Green red lentils', 'Carrot', 'Onion', 'Small Zucchini', 'Coriander', 'Spinach', 'Lasagna sheets', 'Vegan butter', 'Flour', 'Soya milk', 'Mustard', 'Vinegar', null,null, null, null, null, null, null, null,
-  '1 cup', '1', '1', '1', '1', '150g', '10', '35g', '4 tbsp', '300ml', '1.5 tsp', '1 tsp', null, null, null, null, null, null, null, null,
-   FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, FALSE),
+ (8, 2, '20 minutes', 2, 'salmon-salad.jpg', 
+ 'Salmon Avocado Salad',
+ 'Season the salmon, then rub with oil.
+Mix the dressing ingredients together.',
+ 'Halve, stone, peel and slice the avocados.
+Halve and quarter the cucumber lengthways, then cut into slices.',
+ 'Divide salad, avocado and cucumber between four plates, then drizzle with half the dressing.
+Heat a non-stick pan.',
+ 'Add the salmon and fry for 3-4 mins on each side until crisp but still moist inside.',
+  'Put a salmon fillet on top of each salad and drizzle over the remaining dressing.
+Serve warm.', 
+ 'Salmon', 'Avocado', 'Cucumber', 'Spinach', 'Mint', 'Lime', 'Honey', 'Olive oil', null, null, null, null, null,null, null, null, null, null, null, null,
+  '400g', '3', '1', '400g', '4 tbsp', 'Zest and juice of 1', '2 tsp', '3 tbsp', null, null, null, null, null, null, null, null, null, null, null, null,
+   FALSE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE),
  
  (9, 3, '5 minutes', 1, 'overnightOats.jpg', 
  'Vegan Overnight Oats',
@@ -119,16 +120,16 @@ Drain and serve with sauce.',
   TRUE, TRUE, FALSE, TRUE, FALSE, FALSE, FALSE, FALSE),
  
 
- (10, 3,'45 minutes', 2, 'beans.jpg', 
- 'BEANS',
- 'Cube a sweet potato and place on baking sheet with cinnamom and olive oil at 350F for 40mins. Flip once at 20mins.',
- 'Chop a medium yellow onion and 3 gloves of garlic and place in hot brazier at medium heat. Cook until onions are translucent. Start your desired amount of rice.',
- 'Add salt, pepper, coriander, cumin, cayenne, chili powder, and tumeric and cook for another min. Add your choice of greens and pinto beans',
- 'Once the sweet potatos and greens are done. Place your tortillas in the oven for 30seconds. Prepare your sauces. My preference is sirracha-mayo or guacamole.',
- 'Serve.',
- 'Sweet Potato', 'Yellow Onion', 'Garlic Cloves', 'Pinto Beans', 'Red Chard', 'Tortillas', 'Salt', 'Pepper', 'Cayenne', 'Chili Powder', 'Tumeric', 'Coriander', 'Cumin', 'Mayo', 'Siraccha', null, null, null, null, null,
-  '2 Medium', '1 Medium', '3', '1 can', '1 bunch', '4', '1 TBSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '1 TSP', '3 TBSP', '1 TBSP', null, null, null, null, null,
-   TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE),
+ (10, 3,'30 minutes', 4, 'kung-pao.jpg', 
+ 'Kung Pao Chicken',
+ 'Place the oil in a shallow pan over a medium heat. Add in the garlic, chilli, ginger and chicken.',
+ 'Saute for 5 minutes. Add in the spring onions and peanuts.',
+ 'Stir fry for 5 minutes.',
+ 'Whisk together the ingredients for the sauce and pour into the pan with the chicken. Simmer the sauce for 3-5 minutes.',
+ 'Serve with rice and top with extra spring onions and chilli flakes.',
+ 'Cooking oil', 'Garlic cloves', 'Chilli flakes', 'Fresh ginger', 'Chicken breasts', 'Spring onions', 'Peanuts', 'Brown sugar', 'Rice wine vinegar', 'Tamari', 'Chicken stock', 'Cornflour', null, null, null, null, null, null, null, null,
+  '1 tbsp', '2', '1/4 tsp', '1 tsp', '2', '4', '1 tbsp', '2 tbsp', '1 tbsp', '2 tbsp', '100 ml', '1 tbsp', null, null, null, null, null, null, null, null,
+   FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, TRUE),
 
   (11, 2, '10 minutes', 2, 'muesli.jpg', 
  'Spa Day Muesli',
