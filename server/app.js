5d15afc18562799911710b18a6c220ffab4f530e
Guy Dilena <guydilena@gmail.com>
2022-05-18 11:33:41 -0300

    Added: Twilio stuff not working

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const cookieParser = require("cookie-parser");
+// const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const cors = require("cors");
 const morgan = require("morgan");
@@ -14,7 +14,7 @@ app.use(morgan("dev"));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   fileUpload({
